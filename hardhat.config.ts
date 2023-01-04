228fb0ed3d5d9a5de738dd9fbe7ae2b2650a0426
ebadiere <ebadiere@gmail.com>
2023-01-04 16:47:56 -0700

    Updated deploy script to use wallet and deploy to local running node.

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,20 @@
-import "dotenv/config.js";
+import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+// import path, { resolve } from "path";
+import "tsconfig-paths/register";
+
+dotenv.config();
+console.log("Config: ", dotenv.config());
+console.log("Private Key: ", process.env.PRIVATE_KEY);
+
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    mainnet: {
-      url: process.env.INFURA_API_KEY
+    infura: {
+      url: process.env.INFURA_API_KEY,
+      accounts: [ "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" ]
     }
   }
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,26 @@
-import { ethers } from "hardhat";
+import {ethers} from "hardhat";
 
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
+  const nodeUrl = process.env.NODE_URL;
+  if (!nodeUrl){
+    console.log("Node URL not set!");
+    process.exit(1);
+  }
+  const provider = ethers.getDefaultProvider(nodeUrl);
+  const privateKey = process.env.PRIVATE_KEY;
+  if (!privateKey){
+    console.log("Private key not set!");
+    process.exit(1);
+  }
+  const wallet = new ethers.Wallet(privateKey, provider);
   const lockedAmount = ethers.utils.parseEther("1");
+  const account = wallet.connect(provider);
 
-  const Lock = await ethers.getContractFactory("Lock");
+  const Lock = await ethers.getContractFactory("Lock", account);
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
   await lock.deployed();
